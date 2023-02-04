9fca6d60f9cdd88529efa8939b8a648b710881b9
nathan.pioucraft@outlook.com <nathan.pioucraft@outlook.com>
2023-02-04 08:52:01 +0100

    add deployement server

--- server/main.py
+++ server/main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from duckduckgo_search import ddg
+import waitress
 app = Flask(__name__)
 @app.route('/api/search/<lang>/<query>')
 def getInfo(lang, query):
@@ -11,4 +12,6 @@ def getInfo(lang, query):
 
 
 
-app.run()
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
