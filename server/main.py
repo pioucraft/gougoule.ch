996748196f6d005f172e625dfc422b638f9a44f9
nathan.pioucraft@outlook.com <nathan.pioucraft@outlook.com>
2023-02-04 08:42:33 +0100

    change  from gooogle results to duckduckgo results

--- server/main.py
+++ server/main.py
@@ -1,23 +1,12 @@
-from googlesearch import search
 from flask import Flask
-import wikipedia
+from duckduckgo_search import ddg
 app = Flask(__name__)
 @app.route('/api/search/<lang>/<query>')
 def getInfo(lang, query):
-    result = {}
-    result["google"] = {}
-    result["wikipedia"] = {}
-    for i in search(query, tld="com", num=10, stop=10, lang=lang, safe="off"):
-        result["google"][len(result["google"])] = {}
-        result["google"][len(result["google"]) - 1]["url"] = i
-    try:
-        wikipedia.set_lang(lang)
-        result["wikipedia"]["summary"] = wikipedia.summary(wikipedia.suggest(query), sentences=3)
-        result["wikipedia"]["url"] = str(wikipedia.page(wikipedia.suggest(query)).url)
-    except:
-        result["wikipedia"]["summary"] = None
-        result["wikipedia"]["url"] = None
-    return result
+    results = {}
+    results["duckduckgo"] = {}
+    results["duckduckgo"] = ddg(query, region=f"ch-{lang}", max_results=15)
+    return results
 
 
 
