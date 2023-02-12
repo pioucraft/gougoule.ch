c8b9f60851be855bda6f8ad5f4cda72280248fdc
nathan.pioucraft@outlook.com <nathan.pioucraft@outlook.com>
2023-02-12 13:36:00 +0100

    initialize the messages thing

--- server/app.py
+++ server/app.py
@@ -1,18 +1,35 @@
 from flask import Flask
 from duckduckgo_search import ddg
 from flask_cors import CORS
-import wikipedia
+import json
+import datetime
+import os
+
+os.chdir(r"/home/emmanuel_macron/Documents/programinc/gougoule.ch/server/")
 app = Flask(__name__)
 CORS(app)
 
 
 @app.route('/api/search/<lang>/<query>')
-def getInfo(lang, query):
+def search(lang, query):
     query = query.replace("+", " ")
     results = {}
     results["duckduckgo"] = {}
     results["duckduckgo"] = ddg(query, region=f"{lang}", max_results=15)
     return results
+
+@app.route('/api/post-message/<token>/<group>/<message>', methods = ['POST'])
+def post_message(token, group, message):
+    with open("data.json", "r") as f:
+        data = json.load(f)
+    messagenb = len(data["groups"][group]["messages"])
+    data["messages"]["groups"][group]["messages"][messagenb] = {}
+    data["messages"]["groups"][group]["messages"][messagenb]["content"] = message
+    result = {}
+    with open("data.json", "w") as f:
+        json.dump(data, f, indent=4)
+    result["response"] = 200
+    return result
 
 
 
