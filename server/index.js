3cf518278f2779e49d7ca116c13bf8026720eb98
pfannkuchen <nathan.pioucraft@outlook.com>
2023-08-12 09:14:40 +0200

    idk

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 
-app.use("cors()")
+app.use(cors())
 
 app.get("/api/get-duolingo-stats/", (req, res) => {
     fetch("https://duome.eu/pfannkuchen31").then(data => data.text()).then(data => res.send(data))
