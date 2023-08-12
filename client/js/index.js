8e0758542b56fb543651ddb88a61238fb94d49b1
pfannkuchen <nathan.pioucraft@outlook.com>
2023-08-12 09:29:20 +0200

    fix duolingo days streak bug

--- client/js/index.js
+++ client/js/index.js
@@ -1,5 +1,5 @@
 fetch("https://gougoule.ch/api/get-duolingo-stats").then(data => data.text()).then(data => {
-    let daysStreak = (data.split(`<div><span class="icon streak"></span> <span class=""><span aria-label="`)[1].split(`">`)[0])
+    let daysStreak = (data.split(`</span> <span class=""><span aria-label="`)[1].split(`">`)[0])
 
     let XP = (data.split(`<div>`)[1].split("</div>")[0])
     document.getElementById("DuolingoStats").innerHTML = `${daysStreak} --- ${XP} Total`
