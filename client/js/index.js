af10c9bd55951bfdfc7022890d5694b0f2717608
pfannkuchen <nathan.pioucraft@outlook.com>
2023-08-12 09:13:47 +0200

    make the duolingo stats working

--- client/js/index.js
+++ client/js/index.js
@@ -1,4 +1,4 @@
-fetch("https://cors-anywhere.herokuapp.com/https://duome.eu/pfannkuchen31").then(data => data.text()).then(data => {
+fetch("https://gougoule.ch/api/get-duolingo-stats").then(data => data.text()).then(data => {
     let daysStreak = (data.split(`<div><span class="icon streak"></span> <span class=""><span aria-label="`)[1].split(`">`)[0])
 
     let XP = (data.split(`<div>`)[1].split("</div>")[0])
